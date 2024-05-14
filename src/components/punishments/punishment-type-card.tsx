4c93b512fa9da19e0930c0a5d177660f5a511d2b
David Villarreal <contacto@yosoyvillaa.com>
2024-05-14 21:15:15 +0200

    fix punishment type card content index

--- src/components/punishments/punishment-type-card.tsx
+++ src/components/punishments/punishment-type-card.tsx
@@ -20,7 +20,7 @@ export const PunishmentTypeCard = ({
     >
       <Card className="overflow-hidden mx-auto w-60 h-36 flex-col flex items-center relative transition ease-in-out hover:scale-110">
         {punishmentIcon}
-        <CardContent className="text-center mt-auto">
+        <CardContent className="text-center mt-auto z-[1]">
           <p className="text-[3.5rem] leading-none font-bold">{count}</p>
           <CardTitle className="text-2xl font-medium">{title}</CardTitle>
         </CardContent>
