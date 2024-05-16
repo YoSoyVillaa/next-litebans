570059a6758504bcb5d83802307d48e68a144d71
David Villarreal <contacto@yosoyvillaa.com>
2024-05-16 16:40:18 +0200

    remove unnecesary types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,3 @@
-export type PageProps = {
-  params: { lang: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export type PageLangProps = {
-  params: { lang: string }
-}
-
 export type SearchParams = {
   searchParams: { [key: string]: string | string[] | undefined }
 }
