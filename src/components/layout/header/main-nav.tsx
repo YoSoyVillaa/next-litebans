a687d1bcef4d972741160031a530cf06cc87980e
David Villarreal <contacto@yosoyvillaa.com>
2025-01-28 16:47:52 +0100

    style: hide punishment count in navbar with smaller screens before phone

--- src/components/layout/header/main-nav.tsx
+++ src/components/layout/header/main-nav.tsx
@@ -37,28 +37,28 @@ export const MainNav = ({ bans, mutes, warns, kicks}: MainNavProps) => {
           href="/bans"
           className={cn("transition-colors", pathname.startsWith("/bans") ? "hover:text-foreground text-foreground/80" : "hover:text-foreground/80 text-foreground/60")}
         >
-          {dictionary.words.bans.plural} <Badge variant={pathname.startsWith("/bans") ? "default" : "secondary"} className="!px-1 !py-0">{bans}</Badge>
+          {dictionary.words.bans.plural} <Badge variant={pathname.startsWith("/bans") ? "default" : "secondary"} className="!px-1 !py-0 hidden lg:inline-flex">{bans}</Badge>
         </Link>
 
         <Link 
           href="/mutes"
           className={cn("transition-colors", pathname.startsWith("/mutes") ? "hover:text-foreground text-foreground/80" : "hover:text-foreground/80 text-foreground/60")}
         >
-          {dictionary.words.mutes.plural} <Badge variant={pathname.startsWith("/mutes") ? "default" : "secondary"} className="!px-1 !py-0">{mutes}</Badge>
+          {dictionary.words.mutes.plural} <Badge variant={pathname.startsWith("/mutes") ? "default" : "secondary"} className="!px-1 !py-0 hidden lg:inline-flex">{mutes}</Badge>
         </Link>
         
         <Link 
           href="/warns"
           className={cn("transition-colors", pathname.startsWith("/warns") ? "hover:text-foreground text-foreground/80" : "hover:text-foreground/80 text-foreground/60")}
         >
-          {dictionary.words.warns.plural} <Badge variant={pathname.startsWith("/warns") ? "default" : "secondary"} className="!px-1 !py-0">{warns}</Badge>
+          {dictionary.words.warns.plural} <Badge variant={pathname.startsWith("/warns") ? "default" : "secondary"} className="!px-1 !py-0 hidden lg:inline-flex">{warns}</Badge>
         </Link>
         
         <Link 
           href="/kicks"
           className={cn("transition-colors", pathname.startsWith("/kicks") ? "hover:text-foreground text-foreground/80" : "hover:text-foreground/80 text-foreground/60")}
         >
-          {dictionary.words.kicks.plural} <Badge variant={pathname.startsWith("/kicks") ? "default" : "secondary"} className="!px-1 !py-0">{kicks}</Badge>
+          {dictionary.words.kicks.plural} <Badge variant={pathname.startsWith("/kicks") ? "default" : "secondary"} className="!px-1 !py-0 hidden lg:inline-flex">{kicks}</Badge>
         </Link>
       </nav>
     </div>
